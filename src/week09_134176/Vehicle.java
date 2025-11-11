a5538b0994a31c9e985064dca321f9abbca8ca4c
aquijade06-bit <aquijade06@gmail.com>
2025-11-11 23:43:01 +0700

    Class Car implements Vehicle

---
+++ src/week09_134176/Vehicle.java
@@ -0,0 +1,23 @@
+package week09_134176;
+
+public class Vehicle {
+	// Nested interface untuk tindakan pada kendaraan
+	interface Action {
+		void start();
+		void stop();
+	}
+	
+	private String name;
+	
+	public Vehicle(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void displayInfo() {
+		System.out.println("Vehicle: " + name);
+	}
+}
